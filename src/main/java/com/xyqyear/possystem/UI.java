c368142ac2a202e6ef213c569cc5fad38741dd25
year xyq <xyqyear@gmail.com>
2021-06-09 10:00:15 +0800

    isolate scanner functionality

--- src/main/java/com/xyqyear/possystem/UI.java
+++ src/main/java/com/xyqyear/possystem/UI.java
@@ -4,25 +4,37 @@ import java.util.Scanner;
 
 public class UI {
     public static void main(String[] args) {
-        run();
+        UI ui = new UI();
+        ui.run();
     }
 
-    private static void run() {
+    private int readInt() {
         Scanner scanner = new Scanner(System.in);
+        int temp = scanner.nextInt();
+        scanner.close();
+        return temp;
+    }
 
+    private String readLine() {
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.nextLine();
+        scanner.close();
+        return temp;
+    }
+
+    private void run() {
         POSSystem pos = new POSSystem();
         System.out.println("正在初始化（加载产品目录）……");
         pos.startUp();
 
         while (true) {
             System.out.println("要开始一次销售吗？(y/n): ");
-            String choice = scanner.nextLine();
+            String choice = readLine();
             boolean continueFlag = false;
             if (choice.equals("y")) {
                 continueFlag = true;
             } else if (choice.equals("n")) {
                 System.out.println("正在退出系统……");
-                scanner.close();
                 System.exit(0);
             } else {
                 System.out.println("请输入y/n!");
@@ -34,15 +46,15 @@ public class UI {
 
                 while (true) {
                     System.out.println("请输入id: ");
-                    int id = scanner.nextInt();
+                    int id = readInt();
                     System.out.println("请输入数量: ");
-                    int quantity = scanner.nextInt();
+                    int quantity = readInt();
                     pos.enterItem(id, quantity);
 
                     boolean continueFlag1 = false;
                     while (true) {
                         System.out.println("要继续加购商品吗？(y/n)");
-                        String choice1 = scanner.nextLine();
+                        String choice1 = readLine();
                         if (choice1.equals("y")) {
                             continueFlag1 = true;
                             break;
@@ -61,7 +73,7 @@ public class UI {
                 }
 
                 System.out.println("打钱!");
-                int paymentAmount = scanner.nextInt();
+                int paymentAmount = readInt();
                 pos.makePayment(paymentAmount);
                 System.out.println("正在打印单据……");
                 pos.finishASale();
