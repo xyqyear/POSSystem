977a0e898aeb903706f56fce16e9d7a321c3bf37
year xyq <xyqyear@gmail.com>
2021-06-14 16:56:36 +0800

    add product catalog db

--- src/main/java/com/xyqyear/possystem/core/ProductCatalog.java
+++ src/main/java/com/xyqyear/possystem/core/ProductCatalog.java
@@ -1,21 +1,34 @@
 package com.xyqyear.possystem.core;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 class ProductCatalog {
-    private Map<Integer, ProductDescription> descriptions = new HashMap<Integer, ProductDescription>();
+    private Connection connection;
 
     public ProductDescription getProductDesc(int id) {
-        return descriptions.get(id);
+        try {
+            ResultSet rs = connection.createStatement().executeQuery("select name, price from catalog where id=" + id);
+            return new ProductDescription(id, rs.getString("name"), rs.getDouble("price"));
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     public ProductCatalog() {
-        loadCatalog();
+        loadDB();
     }
 
-    private void loadCatalog() {
-        ProductDescription desc1 = new ProductDescription(1, "辣条", 2.5);
-        descriptions.put(1, desc1);
+    private void loadDB() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:catalog.sqlite3");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
